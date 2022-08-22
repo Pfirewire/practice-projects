b44c42011326d023f42570a7f2bcaad9cb23b67f
Stephen Pflug <stephen@pflug.us>
2022-08-22 13:07:52 -0500

    set notes parameters and added .js and .json files

---
+++ js/notes.js
@@ -0,0 +1,17 @@
+$(() => {
+    "use strict";
+
+    // 1. pleasing layout
+    // 2. able to save notes locally
+    // 3. title, date and time saved, tag words, body of note
+    // 4. ability to load previous notes by title
+    // 5. ability to save notes
+    // 6. checks note titles to not have duplicates
+    // 7. display notes list with titles
+    // 8. filter notes list by tag word
+    // 9. sort tag list by different parameters
+
+
+
+
+});
