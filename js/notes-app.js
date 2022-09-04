a4b2418752c6f94d8a1a64041ceeb846808fd866
Stephen Pflug <stephen@pflug.us>
2022-09-04 10:04:41 -0500

    set up links and scripts for tip calculator

---
+++ js/notes-app.js
@@ -0,0 +1,30 @@
+$(() => {
+    "use strict";
+
+    // 1. pleasing layout
+    // 2. able to save notes locally
+    // 3. title, date and time saved, tag words, body of note
+    // 4. ability to load previous notes by title
+    // 5. ability to save notes
+    // 6. checks note titles to not have duplicates
+    // 7. display notes list with titles
+    // 8. filter notes list by tag word
+    // 9. sort tag list by different parameters
+
+    //prints out each tag
+    // const eachTag = tags => {
+    //     return tags.join(", ");
+    // }
+    //
+    // $.get("data/notes.json", () => {}).done((data) => {
+    //     $("#note-title").html(data[0].title);
+    //     $("#note-tags").html(eachTag(data[0].tags));
+    //     $("#note-body").html(data[0].body);
+    // });
+    //
+    // $.post("data/notes.json", {
+    //         title: "some title"
+    //     });
+
+    
+});
